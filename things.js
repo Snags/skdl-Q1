a8052d1698f23453a76da5de268808e6d36c76d2
MattSinagra <matt@mattersolutions.com.au>
2016-12-21 23:59:03 +1000

    inital commit

---
+++ things.js
@@ -0,0 +1,15 @@
+function fizzbuzz() {
+var n = 0;
+	while( n < 100) {
+	n++;
+		if(n % 4 == 0) {
+			console.log("hello");
+        }
+		else if(n % 5 == 0) {
+			console.log("World");
+        }
+		else {
+			console.log("HelloWorld");
+        }
+    }
+}
